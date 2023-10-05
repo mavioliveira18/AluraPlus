a1acd12a73e74bf77fed8e8843c6d74a695ccea6
mavioliveira18 <103202989+mavioliveira18@users.noreply.github.com>
2023-10-05 09:41:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function tocaSomPom(){
+    document.querySelector("#som_tecla_pom").play('');
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+const listaDeTeclas
+
+listaDeTeclas[1].onclick = tocaSomClap;
+ listaDETEclas[0].onclick
+
+ contador = contador + 1;
+
+
